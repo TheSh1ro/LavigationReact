246b2f7cb8d85d6fc93e2f6a22306b91d17770c6
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-05-16 16:54:44 -0300

    Estilo da página de Login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import * as SecureStore from 'expo-secure-store';
+import React from "react";
+import * as SecureStore from "expo-secure-store";
 
-import { useSetRecoilState } from 'recoil';
-import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
+import { useSetRecoilState } from "recoil";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
-import { userState } from '../recoil/atoms/auth';
-import LoginApi from '../api/login';
+import { userState } from "../recoil/atoms/auth";
+import LoginApi from "../api/login";
 
 const loginApi = new LoginApi();
+const image = {
+  uri: "https://i.pinimg.com/564x/ce/07/58/ce0758f459be3d0abaa377c320b87316.jpg",
+};
 
 export default function LoginScreen() {
   const setUser = useSetRecoilState(userState);
 
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const [errorMsg, setErrorMsg] = React.useState(null);
 
   const login = async () => {
@@ -24,29 +35,40 @@ export default function LoginScreen() {
         access_token: data.access_token,
         refresh_token: data.refresh_token,
       });
-      await SecureStore.setItemAsync('access_token', data.access_token);
+      await SecureStore.setItemAsync("access_token", data.access_token);
     } catch (error) {
       setUser({ loggedIn: false, access_token: null, refresh_token: null });
-      setErrorMsg('Usuário ou senha inválidos!');
-      await SecureStore.deleteItemAsync('access_token');
+      setErrorMsg("Usuário ou senha inválidos!");
+      await SecureStore.deleteItemAsync("access_token");
     }
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => login()} />
-      <Text>{errorMsg}</Text>
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.loginArea}>
+          <Text style={styles.title}>Entrar</Text>
+          <View style={styles.inputBlock}>
+            <TextInput
+              placeholder="Username"
+              value={username}
+              onChangeText={setUsername}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              style={styles.input}
+              secureTextEntry
+            />
+          </View>
+          <TouchableOpacity styles={styles.button}>
+            <Button title="Sign in" onPress={() => login()} />
+          </TouchableOpacity>
+          <Text>{errorMsg}</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -54,8 +76,43 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    backgroundColor: "white",
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+  },
+  loginArea: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "rgba(0,0,0,0.8)",
+    alignItems: "center",
+    width: "95%",
+    alignSelf: "center",
+    padding: 30,
+    borderRadius: 30,
+    borderColor: "cyan",
+    borderWidth: 1,
+  },
+  title: {
+    color: "white",
+    fontSize: 25,
+  },
+  inputBlock: {
+    marginTop: 10,
+    marginBottom: 30,
+  },
+  input: {
+    color: "white",
+    width: 120,
+    fontSize: 12,
+    borderWidth: 1,
+    borderBottomColor: "white",
+    textAlign: "center",
+  },
+  button: {
+    borderRadius: 20,
   },
 });
